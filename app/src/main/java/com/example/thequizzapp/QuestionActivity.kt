242f9334a3e0311e3c204d112c33655eecbfe31b
vivi <95643711+vivirnogueira@users.noreply.github.com>
2023-03-07 14:49:21 -0300

    1:19:37

--- app/src/main/java/com/example/thequizzapp/QuestionActivity.kt
+++ app/src/main/java/com/example/thequizzapp/QuestionActivity.kt
@@ -17,36 +17,41 @@ class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-        setData.getQuestion()
 
         questionList=setData.getQuestion()
 
         setQuestion()
 
 
-        /*
-        questionList=setData.getQuestion()
-        var que=questionList!![0]
-        question_text.text=que.question
-        opt_1.setOnClickListener{
-            opt_1.background=ContextCompat.getDrawable( this,R.drawable.selected_question_option)
-        }
-        opt_1.text=que.option_one
-        opt_2.text=que.option_two
-        opt_3.text=que.option_three
-        opt_4.text=que.option_four
 
-        progress_horizontal.progress=1
-        progress_horizontal.max=questionList!!.size
-*/
-        /*
-        var demo = setData.getQuestion()
-        Log.e("meu teste", "${demo.size}")
-        */
+
     }
 
     fun setQuestion(){
 
         val question = questionList!![currentPosition-1]
+
+        progress_horizontal.progress=currentPosition
+        progress_horizontal.max=questionList!!.size
+        progress_text="${currentPosition}"+"/"+"${questionList!!.size}"
+        question_text.text=question.question
+        opt_1.text=question.option_one
+        opt_2.text=question.option_two
+        opt_3.text=question.option_three
+        opt_4.text=question.option_four
+    }
+
+    fun setOptionStyle(){
+        var optionList:ArrayList<TextView> = arrayListOf()
+        optionList.add(0,opt_1)
+        optionList.add(1,opt_2)
+        optionList.add(2,opt_3)
+        optionList.add(3,opt_4)
+
+        for(op in optionList)
+        {
+            op.setTextColor(Color.parseColor("#555151"))
+            op.background=ContextCompat.getDrawable(R.drawable.question_option)
+        }
     }
 }
